08754d9c9c1f721f0448d349c0cb5ea28924061c
Dominik Friberg <78792819+friibii@users.noreply.github.com>
2021-11-25 16:56:34 +0100

    Update teams_ProfilName.cmd

--- teams_ProfilName.cmd
+++ teams_ProfilName.cmd
@@ -1,6 +1,4 @@
 @ECHO OFF
-SET MSTEAMS_PROFILE=%~n0
-SET "OLD_USERPROFILE=%USERPROFILE%"
-SET "USERPROFILE=%LOCALAPPDATA%\Microsoft\Teams\CustomProfiles\%MSTEAMS_PROFILE%"
-cd "%OLD_USERPROFILE%\AppData\Local\Microsoft\Teams"
-"%OLD_USERPROFILE%\AppData\Local\Microsoft\Teams\Update.exe" --processStart "Teams.exe"
+SET "UserAppPath=%USERPROFILE%"
+SET "USERPROFILE=%LOCALAPPDATA%\Microsoft\Teams\CustomProfiles\%~n0"
+"%UserAppPath%\AppData\Local\Microsoft\Teams\Update.exe" --processStart "Teams.exe"
